21cfda9a14fed3a63fce74f521ecd3384f2957c8
Barend Erasmus <hirebarend@gmail.com>
2025-03-14 11:11:49 +0100

    add pocket property

---
+++ src/core/forms/pocket-property.form.ts
@@ -0,0 +1,112 @@
+import { subYears } from "date-fns";
+import { IForm } from "../types";
+
+export const FORM_POCKET_PROPERTY: IForm = {
+  id: "pocket-property",
+  image: "https://pocketproperty.app/_next/static/media/logo.f88f19f0.svg",
+  payment: {
+    amount: 225_00,
+  },
+  sections: [
+    {
+      description: null,
+      fields: [
+        {
+          type: "text",
+          name: "applicant_first_name",
+          title: "First Name",
+          description: "Enter your first name as it appears on your ID.",
+          isRequired: true,
+        },
+        {
+          type: "text",
+          name: "applicant_last_name",
+          title: "Last Name",
+          description: "Enter your last name as it appears on your ID.",
+          isRequired: true,
+        },
+        {
+          type: "text",
+          name: "applicant_email_address",
+          title: "Email Address",
+          isRequired: true,
+          inputType: "email",
+        },
+        {
+          type: "text",
+          name: "applicant_mobile_number",
+          title: "Mobile Number",
+          isRequired: true,
+          inputType: "tel",
+        },
+      ],
+      title: "Personal Information",
+    },
+    {
+      description: null,
+      fields: [
+        {
+          type: "text",
+          name: "applicant_date_of_birth",
+          title: "Date of Birth",
+          isRequired: true,
+          inputType: "date",
+          max: subYears(new Date(), 16).toISOString(),
+        },
+        {
+          type: "text",
+          name: "applicant_identity_number",
+          title: "South African ID Number",
+          isRequired: true,
+        },
+        {
+          type: "dropdown",
+          name: "applicant_marital_status",
+          title: "Marital Status",
+          isRequired: true,
+          choices: ["Single", "Married", "Divorced", "Other"],
+        },
+      ],
+      title: "Additional Information",
+    },
+    {
+      description: null,
+      fields: [
+        {
+          type: "file",
+          name: "applicant_documents_identity_document",
+          title: "Identity Document",
+          tags: ["identity-document"],
+          description:
+            "Please upload a photo of your identity document such as your South African Smart ID Card or Green ID Book",
+          isRequired: true,
+          sourceType: "camera",
+        },
+        {
+          type: "photo",
+          name: "applicant_documents_photo",
+          title: "Photo",
+          description: "Please upload a photo of yourself",
+          isRequired: true,
+        },
+      ],
+      title: "Identity Verification",
+    },
+    {
+      description: null,
+      fields: [
+        {
+          type: "signature-pad",
+          name: "signature",
+          title: "Signature",
+          description:
+            "I, hereby consent to <b>Get Verified</b> and its authorized agents to access my credit information for the purpose of assessing my creditworthiness in relation to <b>a rental agreement</b>.<br /><br />I understand that this may involve obtaining my credit report from registered credit bureaus in accordance with the <a href=https://www.gov.za/sites/default/files/gcis_document/201409/a34-050_1.pdf target=_blank>National Credit Act</a> and the <a href=https://www.gov.za/sites/default/files/gcis_document/201409/3706726-11act4of2013popi.pdf target=_blank>Protection of Personal Information Act (POPIA)</a>.",
+          isRequired: true,
+          placeholder: "Sign",
+        },
+      ],
+      title: "Consent",
+    },
+  ],
+  title: "Rental Application",
+};
--- src/core/themes/index.ts
+++ src/core/themes/index.ts
@@ -1,11 +1,13 @@
 import { Theme } from "@mui/material";
 import { THEME_REVO_PROPERTY } from "./revo-property.theme";
 import { THEME_SAMPLE } from "./sample.theme";
+import { THEME_POCKET_PROPERTY } from "./pocket-property.theme";
 // import { THEME_QUAY_1_INTERNATIONAL_REALTY } from "./quay-1-international-realty.theme";
 
 export const THEMES: { [key: string]: Theme } = {
   "get-verified": THEME_SAMPLE,
   // "get-verified": THEME_QUAY_1_INTERNATIONAL_REALTY,
+  "pocket-property": THEME_POCKET_PROPERTY,
   "revo-property": THEME_REVO_PROPERTY,
   "revo-property-individual": THEME_REVO_PROPERTY,
   "revo-property-individual-guarantor": THEME_REVO_PROPERTY,
