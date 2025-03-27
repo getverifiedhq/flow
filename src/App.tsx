0c51bfe57213164bea91c21de1dfe5fbd769eaaf
Barend Erasmus <hirebarend@gmail.com>
2025-03-27 08:29:31 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { MainRoute, SelectorRoute, ThankYouRoute } from "./routes";
+import { MainRoute, ThankYouRoute } from "./routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainRoute />,
-  },
-  {
-    path: "/selector",
-    element: <SelectorRoute />,
   },
   {
     path: "/:formId",
@@ -23,40 +19,6 @@ const router = createBrowserRouter([
     element: <ThankYouRoute />,
   },
 ]);
-
-// const theme = createTheme({
-//   palette: {
-//     primary: { main: "#36a18b" },
-//     secondary: { main: "#d7ece8" },
-//   },
-//   typography: {
-//     fontFamily: ["DM Sans", "sans-serif"].join(","),
-//   },
-// });
-
-// const themeQuay1InternationalRealty = createTheme({
-//   palette: {
-//     background: {
-//       default: "#f6f6f6",
-//     },
-//     primary: { main: "#3c5aa5" },
-//     secondary: { main: "#d7ece8" },
-//     error: { main: "hsl(344, 23%, 58%)" },
-//     success: { main: "#93ad7c" },
-//   },
-//   typography: {
-//     fontFamily: ["DM Sans", "sans-serif"].join(","),
-//   },
-//   components: {
-//     MuiCssBaseline: {
-//       styleOverrides: {
-//         body: {
-//           backgroundColor: "#f6f6f6",
-//         },
-//       },
-//     },
-//   },
-// });
 
 function App() {
   return <RouterProvider router={router} />;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,2 @@
 export * from "./main.route";
-export * from "./selector.route";
 export * from "./thank-you.route";
