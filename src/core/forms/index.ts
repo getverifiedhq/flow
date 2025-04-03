b43b72c2d5f0a6cba8cedc33075b0dbfa30bb11a
Barend Erasmus <hirebarend@gmail.com>
2025-04-03 13:25:06 +0200

    remove revo property

--- src/core/forms/index.ts
+++ src/core/forms/index.ts
@@ -2,9 +2,6 @@ import { IForm } from "../types";
 import { FORM_GET_VERIFIED } from "./get-verified.form";
 import { FORM_POCKET_PROPERTY } from "./pocket-property.form";
 import { FORM_POCKET_PROPERTY_STAGING } from "./pocket-property-staging.form";
-import { FORM_REVO_PROPERTY_GUARANTOR } from "./revo-property-guarantor.form";
-import { FORM_REVO_PROPERTY_INDIVIDUAL_GUARANTOR } from "./revo-property-individual-guarantor.form";
-import { FORM_REVO_PROPERTY_INDIVIDUAL } from "./revo-property-individual.form";
 import { FORM_SAMPLE } from "./sample.form";
 import { FORM_QUAY_1_INTERNATIONAL_REALTY } from "./quay-1-international-realty.form";
 import { FORM_TRAFALGAR } from "./trafalgar.form";
@@ -14,9 +11,6 @@ export const FORMS: Array<IForm> = [
   FORM_POCKET_PROPERTY,
   FORM_POCKET_PROPERTY_STAGING,
   FORM_QUAY_1_INTERNATIONAL_REALTY,
-  FORM_REVO_PROPERTY_INDIVIDUAL,
-  FORM_REVO_PROPERTY_INDIVIDUAL_GUARANTOR,
-  FORM_REVO_PROPERTY_GUARANTOR,
   FORM_SAMPLE,
   FORM_TRAFALGAR,
 ];
